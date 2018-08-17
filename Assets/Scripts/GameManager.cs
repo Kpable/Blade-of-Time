0cb4bea80b184f2f62484283f28dd97849daedf3
lestherreynoso <lhreynos@gmail.com>
2018-08-17 18:12:50 -0400

    timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public TextMeshProUGUI gameTimerText;
+    
+    private Timer gameTimer;
+
+    public float gameDuration = 300f; // 5 Minutes
+
+    private void Awake()
+    {
+        gameTimer = GetComponent<Timer>();
+
+        if (gameTimer != null)
+        {
+            Debug.Log(name + ": Game Timer is present");
+            gameTimer.OnSecondsChanged += HandleTimerSecondsChange;
+            gameTimer.OnTimeUp += HandleTimerEnd;
+
+            gameTimer.Set(gameDuration, true);
+        }
+        else
+        {
+            Debug.LogError(name + ": Game Timer is null");
+        }
+
+        if (gameTimerText) gameTimerText.text = gameDuration.ToString();
+    }
+
+    private void HandleTimerEnd()
+    {
+        Debug.Log(name + ": Time is Up!");
+        gameTimerText.text = "0";
+
+    }
+
+    private void HandleTimerSecondsChange(int seconds)
+    {
+        Debug.Log(name + ": Seconds Changed: " + seconds);
+        gameTimerText.text = seconds.ToString();
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    #region Events 
+
+    public delegate void SecondsChanged(int seconds);
+    public event SecondsChanged OnSecondsChanged;
+
+    public delegate void TimeUp();
+    public event TimeUp OnTimeUp;
+
+    #endregion 
+
+    #region Private Variables
+
+    private bool running;
+    private float duration;
+    private float remainingTime;
+    private float secondsTracker;
+
+    #endregion 
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (running)
+        {
+            // Reduce remainingTime
+            remainingTime -= Time.deltaTime;
+            // Clamp Time so it does not go below 0
+            remainingTime = Mathf.Clamp(remainingTime, 0, remainingTime);
+
+            // If 1 second has passed, alert listeners of the change in seconds
+            if (secondsTracker - remainingTime >= 1 && OnSecondsChanged != null)
+            {
+                OnSecondsChanged((int)Mathf.Ceil(remainingTime));
+                secondsTracker = remainingTime;
+            }
+
+            if (remainingTime <= 0)
+            {
+                TimeExpired();
+            }
+        }
+    }
+
+    public void StartTimer(float duration = 0)
+    {
+        if (duration > 0) Set(duration);
+        running = true;
+    }
+
+    public void Stop()
+    {
+        running = false;
+    }
+
+    public void Set(float duration, bool start = false)
+    {
+        this.remainingTime = duration;
+        this.duration = duration;
+        secondsTracker = duration;
+        if (start) StartTimer();
+    }
+
+    public void Reset()
+    {
+        remainingTime = duration;
+    }
+
+    void TimeExpired()
+    {
+        Stop();
+        if (OnTimeUp != null) OnTimeUp();
+    }
+
+}
