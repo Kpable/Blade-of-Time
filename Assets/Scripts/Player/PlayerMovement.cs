b5746984e938aa69fd967ebffbaae8675afb0e7b
Nick Stokowski <nkstokowski@gmail.com>
2018-08-17 16:12:48 -0400

    Adding some player movement. May need to change up the player assets for a top down view after all

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [HeaderAttribute("Dependencies")]
+    public GameObject player;
+    public GameObject gameManagerObj;
+
+    // Variables that affect the movement of the player
+    [HeaderAttribute("Movement Variables")]
+    public float currentSpeed = 0.0f;
+    public float walkSpeed = 10.0f;
+
+
+    // Variables that affect the rotation of the player
+    [HeaderAttribute("Rotation Variables")]
+    public float cameraRadius = 0.001f;
+
+    void Start () {
+        currentSpeed = walkSpeed;
+    }
+	
+
+	void Update () {
+        Vector3 currentPosition = transform.position;
+
+        // Player Movement
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * currentSpeed;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * currentSpeed;
+
+        transform.Translate(x, y, 0.0f);
+    }
+
+    //Throwing the rotation in the FixedUpdate means it won't be called when TimeScale == 0.
+    void FixedUpdate()
+    {
+        //Player rotation
+        Vector2 playerOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+        // Position of Mouse
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        if (Vector2.Distance(playerOnScreen, mouseOnScreen) >= cameraRadius)
+        {
+            // Angle between player and mouse
+            float angle = Mathf.Atan2(playerOnScreen.y - mouseOnScreen.y, playerOnScreen.x - mouseOnScreen.x) * Mathf.Rad2Deg;
+
+            // Rotate
+            player.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle + 90));
+        }
+    }
+}
